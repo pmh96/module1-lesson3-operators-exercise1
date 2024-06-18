c45a30dad1c5d2b48fd2d69913c0f37ec012ce5a
Pascal Maximilian Haas <pascal.haas@compax.at>
2024-06-18 22:44:11 +0200

    finalize exercise

--- app/src/main/java/com/example/operators/editHere/OperatorLogic.java
+++ app/src/main/java/com/example/operators/editHere/OperatorLogic.java
@@ -29,28 +29,28 @@ public class OperatorLogic {
         // The code executed, if the condition is true, is enclosed in the {} curled
         // brackets!
 
-        // Check if allowed to vote (citizen and at least 16 years old)
-        if (isCitizen && age >= 16) {
+        // Check if allowed to vote
+        if (/*<CONDITION HERE>*/) {
             System.out.println(textsToPrint.getTextForAllowedToVote());
         }
 
-        // Check if allowed to drive (at least 18 years old)
-        if (age >= 18) {
+        // Check if allowed to drive
+        if (/*<CONDITION HERE>*/) {
             System.out.println(textsToPrint.getTextForAllowedToDrive());
         }
 
-        // Check if allowed to drive BUT not vote (18+ years old, not a citizen)
-        if (age >= 18 && !isCitizen) {
+        // Check if allowed to drive BUT not vote
+        if (/*<CONDITION HERE>*/) {
             System.out.println(textsToPrint.getTextForAllowedToDriveButNotVote());
         }
 
-        // Check if allowed to vote BUT not drive (citizen, 16-17 years old)
-        if (isCitizen && age >= 16 && age < 18) {
+        // Check if allowed to vote BUT not drive 
+        if (/*<CONDITION HERE>*/) {
             System.out.println(textsToPrint.getTextForAllowedToVoteButNotDrive());
         }
 
-        // Check if not allowed to do either (not a citizen, under 16)
-        if (age < 16 || (age < 18 && !isCitizen)) {
+        // Check if not allowed to do either
+        if (/*<CONDITION HERE>*/) {
             System.out.println(textsToPrint.getTextForAllowedToDoNothing());
         }
     }
